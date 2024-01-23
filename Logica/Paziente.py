ea4350cb57391625b8e65c6c388aa440cc79e6fc
sara beccerica <sarabeccerica@gmail.com>
2024-01-23 10:01:01 +0100

    Creazione GUI

---
+++ Logica/Paziente.py
@@ -0,0 +1,6 @@
+class Paziente(object):
+
+    def __init__(self, nome, cognome, codiceFiscale):
+        self.nome = nome
+        self.cognome = cognome
+        self.codiceFiscale = codiceFiscale
